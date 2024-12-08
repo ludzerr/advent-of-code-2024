87d2293a284e0bb871c1a7bdba475ffaef6ce8da
Ludovic Berset <ludovic.berset@hes-so.ch>
2024-12-08 13:09:07 +0100

    Day 2 part 2 is done

--- day-02/d2.py
+++ day-02/d2.py
@@ -8,18 +8,28 @@ def open_file(input_file):
             l.append(line.split(' '))
     return l    
 
-def is_incr_ok(list):
+def is_incr_ok(list, rm):
+    l1 = list.copy()
+    if(rm == len(list)):
+        return False
+    if(rm != -1):
+        list.pop(rm)
     for i in range(1, len(list)):
         cmp = int(list[i]) - int(list[i-1])
         if (cmp <= 0 or cmp > 3):
-            return False
+            return is_incr_ok(l1, rm + 1)
     return True
 
-def is_decr_ok(list):
+def is_decr_ok(list, rm):
+    l1 = list.copy()
+    if(rm == len(list)):
+        return False
+    if(rm != -1):
+        list.pop(rm)
     for i in range(1, len(list)):
-        cmp = int(list[i-1]) - int(list[i])
+        cmp = int(list[i - 1]) - int(list[i])
         if (cmp <= 0 or cmp > 3):
-            return False
+            return is_decr_ok(l1, rm + 1)
     return True
 
 
@@ -28,6 +38,6 @@ if __name__ == "__main__":
     f = open_file(input_file)
     cnt = 0
     for line in f:
-        if(is_incr_ok(line) or is_decr_ok(line)):
+        if(is_incr_ok(line, -1) or is_decr_ok(line, -1)):
             cnt += 1
     print("solution: ", cnt)
